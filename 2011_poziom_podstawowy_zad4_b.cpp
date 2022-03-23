93a94f2e0eb9ede110ae662d336de8c1330e347b
slo-1c-12-2021 <81392436+slo-1c-12-2021@users.noreply.github.com>
2022-03-23 08:38:26 +0100

    Add files via upload

---
+++ 2011_poziom_podstawowy_zad4_b.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    czy_palindrom
+    string s;
+    ifstream f("hasla.txt");
+    while (f>>s){
+     if(s.size()% 2 == 0)licznik++;
+    }
+    cout<< "odp.a= " << licznik << "\n";
+}
